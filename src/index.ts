0fd75b5ff836f8d3e780bc8572697982744c1ab8
Vip <vwetoria@gmail.com>
2023-11-27 03:51:48 +0800

    增加vue demo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import {
   Plugin,
+  Menu,
+  getFrontend,
 } from "siyuan";
 import "@/index.scss";
+import { addDemoTopBarIcon } from './modules/Demo';
+
+let plugin = null
+export function usePlugin(pluginProps?) {
+  if (pluginProps) {
+    plugin = pluginProps
+  }
+  if (!plugin && !pluginProps) {
+    console.error('need bind plugin')
+  }
+  return plugin;
+}
 
 export default class PluginSample extends Plugin {
-  async onload() {
+  private isMobile: boolean;
 
+  async onload() {
+    const frontEnd = getFrontend();
+    this.isMobile = frontEnd === "mobile" || frontEnd === "browser-mobile";
+
+    usePlugin(this);
+    addDemoTopBarIcon()
   }
 }
